3163275918d410e55125db16e64d264e3a2e910a
Giovanni Gargiulo <gargiulo.gianni@gmail.com>
2022-03-11 21:35:32 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
